bf18bf3b35f4af0f830e145ec2b345a75556b145
skywills <william_kcool@hotmail.com>
2017-09-29 12:07:03 +0800

    initial commit

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var port = 3333;
 // create a server
 var server = jayson.server({
     miner_getstat1: function(args, callback) {
-    callback(null, {"result": ["9.3 - ETH", "21", "182724;51;0", "30502;30457;30297;30481;30479;30505", "1827240;100;0", "305020;304570;302970;304810;304790;305050", "53;71;57;67;61;72;55;70;59;71;61;70", "eth-eu1.nanopool.org:9999;dcr.supernova.org:9999", "0;0;0;0"]});
+    callback(null, '{"result": ["9.3 - ETH", "21", "182724;51;0", "30502;30457;30297;30481;30479;30505", "1827240;100;0", "305020;304570;302970;304810;304790;305050", "53;71;57;67;61;72;55;70;59;71;61;70", "eth-eu1.nanopool.org:9999;dcr.supernova.org:9999", "0;0;0;0"]}\n');
   }
 });
 
 server.tcp().listen(port);
 console.log('Tcp Server is running..');
-
+/*
 var client = jayson.client.tcp({
   port: port
 });
@@ -19,10 +19,10 @@ var client = jayson.client.tcp({
 // invoke "add"
 client.request('miner_getstat1', null, 0, function(err, response) {
   if(err) throw err;
-  console.log('result returned....');
- // console.log(response.result); // 2
+  
+  console.log(response.result); // 2
 });
-
+*/
 
 /*
 setInterval(function() {
